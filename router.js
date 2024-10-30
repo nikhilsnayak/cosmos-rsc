16ae90322474a8497e3dc2abaf189efca56497ed
Nikhil S <nikhilsnayak3473@gmail.com>
2024-10-30 09:41:08 +0530

    improve router

--- router.js
+++ router.js
@@ -1,21 +1,35 @@
 'use client';
-import { createContext, startTransition, useState, use } from 'react';
+
+import {
+  createContext,
+  startTransition,
+  useState,
+  use,
+  useEffect,
+} from 'react';
 import { createFromFetch } from 'react-server-dom-webpack/client';
+import { ErrorBoundary } from './error-boundary';
+
+const RouterContext = createContext(null);
+
+export function useRouter() {
+  const context = use(RouterContext);
+  if (context === null) {
+    throw new Error('Router was not mounted');
+  }
+  return context;
+}
 
 const { pathname, search } = new URL(window.location.href);
-
 const initialContent = createFromFetch(
   fetch(`${pathname}${search ? `${search}&_rsc=true` : '?_rsc=true'}`)
 );
 
-const RouterContext = createContext(null);
-
-export function Router() {
+function RouterProvider() {
   const [content, setContent] = useState(initialContent);
 
-  const push = (url) => {
+  const update = (url) => {
     startTransition(() => {
-      window.history.pushState(null, null, url);
       const { pathname, search } = new URL(url, window.location.origin);
       const nextContent = createFromFetch(
         fetch(`${pathname}${search ? `${search}&_rsc=true` : '?_rsc=true'}`)
@@ -23,6 +37,21 @@ export function Router() {
       setContent(nextContent);
     });
   };
+
+  const push = (url) => {
+    window.history.pushState(null, null, url);
+    update(url);
+  };
+
+  useEffect(() => {
+    const handlePopState = () => {
+      const currentUrl = window.location.pathname + window.location.search;
+      update(currentUrl);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+    return () => window.removeEventListener('popstate', handlePopState);
+  }, []);
 
   return (
     <RouterContext
@@ -35,11 +64,10 @@ export function Router() {
   );
 }
 
-export function useRouter() {
-  const context = use(RouterContext);
-  if (context === null) {
-    throw new Error('Router was not mounted');
-  }
-
-  return context;
+export function Router() {
+  return (
+    <ErrorBoundary>
+      <RouterProvider />
+    </ErrorBoundary>
+  );
 }
