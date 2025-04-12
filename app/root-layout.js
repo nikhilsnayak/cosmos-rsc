7332b4186561f3801e573d779e5f5083a430222b
Nikhil S <nikhilsnayak3473@gmail.com>
2025-04-12 07:40:48 +0000

    tailwind

--- app/root-layout.js
+++ app/root-layout.js
@@ -5,6 +5,7 @@ export default function RootLayout({ children }) {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>COSMOS RSC</title>
+        <link rel='stylesheet' href='style.css' />
       </head>
       <body>{children}</body>
     </html>
